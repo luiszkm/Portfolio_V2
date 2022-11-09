67bc102581340808f8a02107f3e436ea53cfe69d
luis <luiszkm@gmail.com>
2022-11-09 16:31:37 -0300

    finished day

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,25 @@
-import { AiFillGithub, AiOutlineLinkedin, AiOutlineInstagram ,AiOutlineFileDone} from 'react-icons/ai'
+import { AiFillGithub, AiOutlineLinkedin, AiOutlineInstagram, AiOutlineFileDone } from 'react-icons/ai'
 import { Avatar } from './Avatar'
 
 
 
-export function Card (){
-  return(
+export function Card() {
+  return (
     <div>
       <Avatar />
-      <span>Desenvolvedor Web </span>
+      <div className='flex flex-col'>
+      <span>Desenvolvedor Web <span>JavaScrip</span>|<span>TypeScript</span>  </span>
+      
+      </div>
+     
 
 
-    
+
       <nav className='flex items-center gap-4'>
-       <a href="#"><AiFillGithub /></a>
-       <a href="#"><AiOutlineLinkedin /></a>
-       <a href="#"><AiOutlineInstagram /></a>
-       <a href="#"><AiOutlineFileDone /></a>
+        <a href="#"><AiFillGithub /></a>
+        <a href="#"><AiOutlineLinkedin /></a>
+        <a href="#"><AiOutlineInstagram /></a>
+        <a href="#"><AiOutlineFileDone /></a>
       </nav>
     </div>
   )
